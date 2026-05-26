c9be871f5d10631e0fdad090e143808ab3034532
abdurahimabrorov593-debug <abdurahimabrorov593@gmail.com>
2026-05-26 14:47:20 +0300

    Add initial FreezeTrade.lua file

---
+++ FreezeTrade.lua
@@ -0,0 +1,27 @@
+Files
+Go to file
+t
+T
+.gitignore
+LICENSE
+README.md
+dr
+/
+FreezeTrade.lua
+in
+main
+
+Edit
+
+Preview
+Indent mode
+
+Spaces
+Indent size
+
+2
+Line wrap mode
+
+No wrap
+Editing FreezeTrade.lua file contents
+  1
